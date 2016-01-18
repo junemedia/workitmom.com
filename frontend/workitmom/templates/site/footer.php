f49f521292aa51a39951d4bb91be62ecc9ffafc7
John Shearer <john@ultranaut.com>
2016-01-18 14:35:53 -0500

    Add Swoop code

--- frontend/workitmom/templates/site/footer.php
+++ frontend/workitmom/templates/site/footer.php
@@ -107,5 +107,30 @@ if(document.location.protocol=='http:'){
 }
 </script>
 <!-- END SiteCTRL Script -->
+<!-- +SWOOP -->
+  <script type="text/javascript">
+  (function addSwoopOnce(domain) {
+    var win = window;
+    try {
+      while (!(win.parent == win || !win.parent.document)) {
+        win = win.parent;
+      }
+    } catch (e) {
+      /* noop */
+    }
+    var doc = win.document;
+    if (!doc.getElementById('swoop_sdk')) {
+      var serverbase = doc.location.protocol + '//ardrone.swoop.com/';
+      var s = doc.createElement('script');
+      s.type = "text/javascript";
+      s.src = serverbase + 'js/spxw.js';
+      s.id = 'swoop_sdk';
+      s.setAttribute('data-domain', domain);
+      s.setAttribute('data-serverbase', serverbase);
+      doc.head.appendChild(s);
+    }
+  })('SW-10152718-5');
+</script>
+<!-- -SWOOP -->
 </body>
 </html>
