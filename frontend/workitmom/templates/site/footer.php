8f38a97f334e4fca4038ce1490247ecebf545ef5
John Shearer <john@ultranaut.com>
2018-10-02 10:48:44 -0400

    Remove Swoop ad unit

--- frontend/workitmom/templates/site/footer.php
+++ frontend/workitmom/templates/site/footer.php
@@ -52,7 +52,6 @@
 </div><!-- /#ad-header -->
 
 <?php include BLUPATH_TEMPLATES.'/site/ads/tynt.php'; ?>
-<?php include BLUPATH_TEMPLATES.'/site/ads/swoop.php'; ?>
 <?php include BLUPATH_TEMPLATES.'/site/ads/underdog.php'; ?>
 <?php include BLUPATH_TEMPLATES.'/site/ads/pubexchange_js.php'; ?>
 
