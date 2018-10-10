61c5c7cb8dd05050140cf1bd33478e42fa02fc24
John Shearer <john@ultranaut.com>
2018-10-10 09:56:54 -0400

    Remove Underdog Media ad unit

--- frontend/workitmom/templates/site/ads/underdog.php
+++
@@ -1,3 +0,0 @@
-<!--- UNDERDOGMEDIA EDGE EDGE_WorkItMom.com JavaScript ADCODE START--->
-<script data-cfasync="false" language="javascript" async src="//udmserve.net/udm/img.fetch?sid=9510;tid=1;ev=1;dt=6;"></script>
-<!--- UNDERDOGMEDIA EDGE EDGE_WorkItMom.com JavaScript ADCODE END--->
